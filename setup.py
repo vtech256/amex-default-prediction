4232e1927a33e8937c143ad9d44163da5315bcd7
vtech256 <victor@naidu.email>
2022-07-30 18:47:46 -0700

    Apply cookiecutter to initialize project
    Source: https://github.com/clinejc/cookiecutter-data-science

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Solution to predict the probability that a customer does not pay back their credit card balance amount in the future based on their monthly customer profile.',
+    author='Victor Naidu (Venkatesh Naidu)',
+    license='MIT',
+)
